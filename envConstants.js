efdc375270ed476354f36945da463940ec56d357
hahazexia <zhouzx@suirui.com>
2021-01-13 10:37:08 +0800

    删除electron编译文件

---
+++ envConstants.js
@@ -0,0 +1,8 @@
+
+const windowConstants = {
+    mainWindow: `file://${__dirname}/page/main.html`
+}
+
+export {
+    windowConstants
+}
---
+++ windows.js
@@ -0,0 +1,39 @@
+import { BrowserWindow } from 'electron';
+import { windowConstants } from './envConstants'
+
+function createMainWindow () {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 500,
+        center: true,
+        resizable: false,
+        frame: false,
+        hasShadow: true,
+        show: false,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true
+        }
+    });
+
+    global.mainWindow = mainWindow;
+    mainWindow.loadURL(windowConstants.mainWindow);
+
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools({mode: 'bottom'});
+    }
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', () => {
+        global.mainWindow = null;
+    });
+
+    return mainWindow;
+}
+
+export {
+    createMainWindow
+}
