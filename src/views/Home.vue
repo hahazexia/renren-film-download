a7176993f173731a87be17860b5d50693e959cd1
hahazexia <zhouzx@suirui.com>
2021-01-14 18:15:17 +0800

    修改页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
-        首页
+        <input type="text" class="search-input" :class="{'open-search-input': openInputClass}">
+        <span @click="openSearchInput">search</span>
     </div>
 </template>
 
@@ -8,10 +9,44 @@
 
 export default {
     name: 'Home',
-
+    data () {
+        return {
+            openInputClass: false
+        }
+    },
+    methods: {
+        openSearchInput () {
+            this.openInputClass = !this.openInputClass;
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.home {
+    .search-input {
+        font-family: inherit;
+        font-size: 12px;
+        line-height: 14px;
+        display: inline-block;
+        box-sizing: border-box;
+        width: 300px;
+        padding: 0.05em 0;
+        color: #eaba82;
+        border: none;
+        border-bottom: 1px solid;
+        outline: none;
+    }
+
+    .search-input {
+        transform: scale3d(0,1,1);
+        transform-origin: 0% 50%;
+        transition: transform 0.3s;
+    }
+    .open-search-input {
+        transform: scale3d(1,1,1);
+        transition-duration: 0.5s;
+    }
+}
 
 </style>
